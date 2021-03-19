5e6166ff4b443c7c1ad3b63b4b3820d059b34a24
dooooor0 <68139125+door0@users.noreply.github.com>
2021-03-19 11:34:31 +0900

    2021-03-19-6

--- js/main.js
+++ js/main.js
@@ -6,36 +6,36 @@ let inputButton = document.querySelector(".input_btn"); //input_btn을 inputButt
 inputButton.addEventListener("click", addMemo); //input_btn을 누르면 addMemo 함수 실행
 
 function addMemo() {
-    let memo = document.querySelector(".input_list").value; //input_list클래스에 값 받아와서 memo 변수에 할당
-    if (memo != null) { //memo가 null이 아닐때 
-        memoList.push(memo); 
-        document.querySelector(".input_list").value = "";
-        document.querySelector(".input_list").focus(); //push한 다음 value 삭제, focus 적용
+    let item = document.querySelector(".item").value; //item클래스에 값 받아와서 item 변수에 할당
+    if (item != null) { //memo가 null이 아닐때 
+        itemList.push(item);
+        document.querySelector(".item").value = "";
+        document.querySelector(".item").focus(); //push한 다음 value 삭제, focus 적용
     }
     showList();
 }
 
-function shoqList() {
+function showList() {
     let list = "<ul>"; //list에 할일 표시
-    for(let i = 0; i < memoList.length; i++) {
-        list += "<li>" + memoList[i] + "<span class='close' id=" + i + ">" + "\u00D7"  + "</span></li>";
+    for(let i = 0; i < itemList.length; i++) {
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">" + "\u00D7"  + "</span></li>";
     }
     list += "</ul>";
-    document.querySelector(".memo_list").innerHTML = list;
+    document.querySelector(".item__list").innerHTML = list;
 
     let deleteButton = document.querySelectorAll(".close"); //클래스가  close인 모든 요소를 가져와서 deleteButton에 저장
     for( let i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].addEventListener("click", deleteMemo); //클릭할 경우 해당 item 삭제(deleteMemo 함수 실행)
+        deleteButton[i].addEventListener("click", deleteItem); //클릭할 경우 해당 item 삭제(deleteMemo 함수 실행)
     }
 }
 
-function deleteMemo() {
+function deleteItem() {
     let id = this.getAttribute("id");
-    memoList.splice(id, 1); //splice 함수를 사용하여 선택한 id부터 1만큼 요소 삭제
+    itemList.splice(id, 1); //splice 함수를 사용하여 선택한 id부터 1만큼 요소 삭제
     showList();
 }
 
-let CheckList = document.querySelector(".memo_list");
+let CheckList = document.querySelector(".item__list");
 CheckList.addEventListener("click", event => {
     if(event.target.tagName === "LI") {
         event.target.classList.toggle('checked'); 
